b7b5a0752758cc2d2418fcbb70d5221ee40ea1ea
fatemehafvaj <fatemehafvaj1996@gmail.com>
2022-07-05 00:17:22 +0430

    changecard

--- lib/screens/home/widget/mentors_item.dart
+++ lib/screens/home/widget/mentors_item.dart
@@ -12,102 +12,88 @@ class MentorItem extends StatelessWidget {
       child: GestureDetector(
         onTap: () {
           Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => DatailPage(mentors))
-              );
+              MaterialPageRoute(builder: (context) => DatailPage(mentors)));
         },
-        child: Card(
-          // elevation: 1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
+        child: Stack(children: [
+          Container(
+            alignment: Alignment.center,
+            height: 250,
+            width: 160,
+            margin: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                // color:context.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage(mentors.imageUrl))),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Card(
-                child: Stack(
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              width: 160,
+              margin: EdgeInsets.only(left: 5, right: 10, bottom: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xff302d2c),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
                   children: [
-                    Container(
-                      margin: EdgeInsets.all(1),
-                      // margin give to pic
-
-                      height: 250,
-                      width: 160,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        image: DecorationImage(
-                            image: AssetImage(mentors.imageUrl),
-                            fit: BoxFit.fitHeight),
-                      ),
-                    ),
-                    Positioned(
-                      left: 8,
-                      right: 8,
-                      top: 150,
-                      // bottom: -50,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xff302d2c),
-                          borderRadius: BorderRadius.circular(10.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          mentors.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 15,
+                              height: 1.5),
                         ),
-                        constraints:
-                            BoxConstraints(maxWidth: 300, maxHeight: 200),
-                      ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          mentors.subtitle,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xffbcbbbb),
+                              fontSize: 15,
+                              height: 1.5),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              mentors.price,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  height: 1.5),
+                            ),
+                            SizedBox(
+                              width: 50,
+                            ),
+                            Container(
+                                child: Icon(
+                              Icons.favorite,
+                              color: Colors.black,
+                              size: 20,
+                            )),
+                          ],
+                        )
+                      ],
                     ),
-                    Positioned(
-                      top: 170,
-                      left: 12,
-                      right: 20,
-                      child: Text(
-                        mentors.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 15,
-                            height: 1.5),
-                      ),
-                    ),
-                    Positioned(
-                      top: 190,
-                      left: 12,
-                      right: 20,
-                      child: Text(
-                        mentors.subtitle,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xffbcbbbb),
-                            fontSize: 15,
-                            height: 1.5),
-                      ),
-                    ),
-                    Positioned(
-                      top: 210,
-                      left: 12,
-                      right: 50,
-                      child: Text(
-                        mentors.price,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 15,
-                            height: 1.5),
-                      ),
-                    ),
-                    Positioned(
-                        top: 210,
-                        right: 20,
-                        bottom: 20,
-                        child: Container(
-                            child: Icon(
-                          Icons.favorite,
-                          color: Colors.black,
-                          size: 20,
-                        )))
                   ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+        ]),
       ),
     );
   }
